eb61559bf8026e73f1863441b21c05272029a219
noamyech <97678611+noamyech@users.noreply.github.com>
2022-01-13 21:00:57 +0200

    Update traning_02_strimlit.py

--- traning_02_strimlit.py
+++ traning_02_strimlit.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 10 21:14:25 2022
+
+@author: Noam
+"""
 import pandas as pd
 import numpy as np
 import altair as alt
@@ -5,15 +11,39 @@ import streamlit as st
 
 header = st.container()
 container1 = st.container()
-	@@ -38,12 +41,88 @@
+container2 = st.container()
+container3 = st.container()
+
+with header:
+    st.title('Streamlit is actually pretty cool!')
+    st.header("Here I'll show you why")
+with container1:
+    "you can actually insert equations:"
+    st.latex("\Delta^2 f/\dot\phi\cdot A=\int a x^2\,dx")
+with container2:
+    "or if you are in a mood for party"
+    clicked=st.button("release baloons")
+    if clicked:
+        st.balloons()
+    st.header('Time to face yourself')
+    "are you a robot?"
+    checked=st.checkbox("I think that I am not a robot")
+    if checked:
+        st.write("Wrong! :monkey:")
+        '**Try again**'
+        choice=st.radio('what are you?',['None','robot','loser'])
+        if choice=='robot':
+            "*** NOPE ***"
+        if choice=='loser':
              "Damm right you are :heart:"
 with container3:
     '''
-    # data visualization
+    # you can do data visualization
+
     you can dislay the table:_.
     '''
     df = pd.DataFrame(np.random.normal(loc=10.0, scale=5.2, size=[600,4]),columns=['a', 'b', 'c','d'])
     c = alt.Chart(df).mark_circle().encode(
     x='a', y='b', size='c', color='d', tooltip=['a', 'b', 'c','d'])
     df
-    st.write(c) 
+    st.write(c)
