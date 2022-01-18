cd741f91c1bbdd0592e6fb0f3a3470bf9b3b4f6a
noamyech <97678611+noamyech@users.noreply.github.com>
2022-01-18 07:28:24 +0200

    Update traning_02_strimlit.py

--- traning_02_strimlit.py
+++ traning_02_strimlit.py
@@ -136,7 +136,8 @@ st.write("""
 st.line_chart(volume_df)
 
 st.header("next: scikit learn mini tutorial")
-from sklearn.datasets import load_boston()
+from sklearn.datasets import load_boston
+
     
     
     
